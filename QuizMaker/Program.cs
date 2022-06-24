370c5288ed44e81337de0145db28236001a25a1d
Rida Serroukh <rida.serroukh@gmail.com>
2022-06-24 22:13:45 +0100

    Removed comments.

--- QuizMaker/Program.cs
+++ QuizMaker/Program.cs
@@ -6,29 +6,11 @@ namespace QuizMaker
         static void Main(string[] args)
         {
             UiMethods.WelcomeMessage();
-
-
-            //Console.WriteLine("Select the game mode to play [P to play] or [A to add questions]");
-            //string gameChoice = Console.ReadLine().ToUpper();
+                       
 
             UiMethods.PromptToSelectGameChoice();
-
+    
             
-                
-            //if (gameChoice == "P")
-            //    {
-            //        UiMethods.GamePlayChoice(UiMethods.GameMode.Play);
-            //    }
-            //    if (gameChoice == "A")
-            //    {
-            //        UiMethods.GamePlayChoice(UiMethods.GameMode.AddQuestion);
-            //        UiMethods.CreateQuestions();
-            //    }
-            //    else
-            //    {
-            //        Console.WriteLine("Please select a valid option");
-            //    }
-
 
             List <QnA> QuestionList = LogicMethods.GenerateQnAList();
             
